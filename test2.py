dfe83f00a4ce582188fef450553fc1500404a31e
Arpad Molnar <arpad.molnarx@gmail.com>
2017-06-20 16:41:12 +0200

    i have no words for it

--- test2.py
+++ test2.py
@@ -2,4 +2,11 @@ for k in range(9):
     for i in range(3):  # horizontal check
         wincount = 0
         for j in range(3):
-            print("k: ", k, "i: ", i+(k//3)*3, "j: ", (j+(k%3)*3))
+            print("k: ", k, "i: ", i + (k // 3) * 3, "j: ", (j + (k % 3) * 3))
+
+
+print("Ujszar")
+
+for i in range(81):
+    print("k: ", i // 9, "i: ", i // 3 - ((i // 9) * 3) + ((i // 27) * 3), "j: ",
+          i - (i // 3 - ((i // 9) * 3) + ((i // 27) * 3)) * 3 - (i // 9) * 6)
