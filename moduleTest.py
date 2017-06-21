fb2a1188144d32cc5b60d589363fb3afb81ed5e4
Arpad Molnar <arpad.molnarx@gmail.com>
2017-06-21 15:02:16 +0200

    both wincheck working

--- moduleTest.py
+++ moduleTest.py
@@ -5,47 +5,62 @@ from tkinter import messagebox
 import sys
 
 calctab = np.array([[0] * 9] * 9)
-
+calctabsmall = np.array([[0] * 3] * 3)
 playfield = np.array([[" "] * 9] * 9)
-
-# for i in range(9):
-#     for j in range(9):
-#         calctab[i, j] = randint(-1, 1)
-
-# print(calctab)
+playfieldsmall = np.array([[" "] * 3] * 3)
 
 
-def wincheck():
+def bigwincheck():
+
+    k, i = None, None
+    x, y = k, i
+    for n in range(81):
+        if n % 3 == 0:
+            wincounth, wincountv, wincountd, wincountd2 = 0, 0, 0, 0
+        k = n // 9
+        i = n // 3 - ((n // 9) * 3) + ((n // 27) * 3)
+        j = n - (n // 3 - ((n // 9) * 3) + ((n // 27) * 3)) * 3 - (n // 9) * 6
+        wincounth = wincounth + calctab[i, j]
+        wincountv = wincountv + calctab[j, i]
+        if k != x or i % 3 == 0:
+            x, y = k, i
+            wincountd = wincountd + calctab[i + n % 3, j]
+            wincountd2 = wincountd2 + calctab[i + n % 3, n // 9 * 3 + abs(j % 3 - 2) - n // 27 * 9]
+
+        if abs(wincounth) == 3 or abs(wincountv) == 3 or abs(wincountd) == 3 or abs(wincountd2) == 3:
+
+            calctabsmall[k // 3, k % 3] = int(wincounth / 3) + int(wincountd / 3) + int(wincountd2 / 3)
+            calctabsmall[k % 3, k // 3] = int(wincountv / 3)
+
+
+def smallwincheck():
     playerwins = False
     if playerwins is False:
-        k, i = None, None
-        x, y = k, i
-        o = 0
-        for n in range(81):
-            if n % 3 == 0:
-                wincounth, wincountv, wincountd, wincountd2 = 0, 0, 0, 0
-            k = n // 9
-            i = n // 3 - ((n // 9) * 3) + ((n // 27) * 3)
-            j = n - (n // 3 - ((n // 9) * 3) + ((n // 27) * 3)) * 3 - (n // 9) * 6
-            wincounth = wincounth + calctab[i, j]
-            wincountv = wincountv + calctab[j, i]
-            if k != x or i % 3 == 0:
-                o = o + 1
-                x, y = k, i
-                wincountd = wincountd + calctab[i + n % 3, j]
-                wincountd2 = wincountd2 + calctab[i + n % 3, n // 9 * 3 + abs(j % 3 - 2) - n // 27 * 9]
-                if n % 3 == 2:
-                    print("n: ", n, "d: ", wincountd, "d2: ", wincountd2, "k: ", k)
-
+        wincountd, wincountd2 = 0, 0
+        for i in range(9):
+            if i % 3 == 0:
+                wincounth, wincountv = 0, 0
+            wincounth = wincounth + calctabsmall[i // 3, i % 3]
+            wincountv = wincountv + calctabsmall[i % 3, i // 3]
+            if i % 3 == 0:
+                wincountd = wincountd + calctabsmall[i // 3, i // 3]
+                wincountd2 = wincountd2 + calctabsmall[i // 3, 2 - i // 3]
             if abs(wincounth) == 3 or abs(wincountv) == 3 or abs(wincountd) == 3 or abs(wincountd2) == 3:
                 playerwins = True
-                print("lofasz")
                 break
+
+        n = 0
+        for i in range(3):
+            for j in range(3):
+                if calctab[i, j] == 0:
+                    n = n + 1
+        if n == 0:
+            return "tie"
 
     if wincounth == 3 or wincountv == 3 or wincountd == 3 or wincountd2 == 3:
         return "x"
     elif wincounth == -3 or wincountv == -3 or wincountd == -3 or wincountd2 == -3:
-        return "o"
+        return "y"
     else:
         return "senki"
 
@@ -69,11 +84,17 @@ def playerturn(player):
                 calctab[i, j] = -1
 
 
-while wincheck() == "senki":
+while smallwincheck() == "senki":
     print(calctab)
+    print(calctabsmall)
     playerturn(1)
+    bigwincheck()
+
     # print(playfield)
-    if wincheck() == "senki":
+    if smallwincheck() == "senki":
         print(calctab)
+        print(calctabsmall)
         playerturn(2)
-        # print(playfield)
+        bigwincheck()
+
+    # # print(playfield)
